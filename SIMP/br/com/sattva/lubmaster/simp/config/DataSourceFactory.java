65d92ec984c44654a9e59d05cc78ebfac8734023
Everton Moura <106688265+moureverton@users.noreply.github.com>
2026-03-08 10:44:54 -0300

    Delete SIMP directory

--- SIMP/br/com/sattva/lubmaster/simp/config/DataSourceFactory.java
+++
@@ -1,9 +0,0 @@
-package br.com.sattva.lubmaster.simp.config;
-
-public class DataSourceFactory {}
-
-
-/* Location:              D:\Evertech\05-LubMaster\LubMaster\SIMP\SATTVA\SIMP.jar!\br\com\sattva\lubmaster\simp\config\DataSourceFactory.class
- * Java compiler version: 7 (51.0)
- * JD-Core Version:       1.1.3
- */
--- SIMP/br/com/sattva/lubmaster/simp/infra/persistence/ControlRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.sattva.lubmaster.simp.infra.persistence;
-
-public class ControlRepository {}
-
-
-/* Location:              D:\Evertech\05-LubMaster\LubMaster\SIMP\SATTVA\SIMP.jar!\br\com\sattva\lubmaster\simp\infra\persistence\ControlRepository.class
- * Java compiler version: 7 (51.0)
- * JD-Core Version:       1.1.3
- */
